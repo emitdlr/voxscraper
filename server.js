38bd455d7d08a9a479cb1025bdacdb0a58181347
emitdlr <emit@utexas.edu>
2018-06-08 13:01:59 -0500

    will I connect

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var request = require("request");
 var cheerio = require("cheerio");
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
 
 var PORT = process.env.PORT || 8080;
